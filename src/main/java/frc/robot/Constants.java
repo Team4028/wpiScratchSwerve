0f2ae8863267a3cd41668a8182b0da1267ea3dc7
WRueter <43909364+WRueter@users.noreply.github.com>
2022-02-08 17:37:16 -0500

    bruhcontroller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -93,6 +93,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
   }
 
   public static final class MK4IModuleConstants {
